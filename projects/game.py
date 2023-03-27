1d66838ba6431801a2e6c2cbadd2a6432d8ec4b5
Vedant <vrsagrawal@gmail.com>
2023-03-27 19:14:29 +0530

    Game modified

--- projects/game.py
+++ projects/game.py
@@ -1,37 +1,76 @@
 import random
 
-colors = ["R", "G", "B", "Y", "W", "O"]
-tries = 10
-code_length = 4
+COLORS = ["R", "G", "B", "Y", "W", "O"]
+TRIES = 10
+CODE_LENGTH = 4
 
 def generate_code():
     code = []
 
-    for _ in range (code_length):
-        color = random.choice(colors)
+    for _ in range (CODE_LENGTH):
+        color = random.choice(COLORS)
         code.append(color)
 
     return code
 
 def guess_code():
-    guess = input("Guess:").upper().split(" ")
+    while True:
+        guess = input("Guess:").upper().split(" ")
 
-    if len(guess) != code_length:
-        print(f"You must guess {code_length} colors.")
-        continue
+        if len(guess) != CODE_LENGTH:
+            print(f"You must guess {CODE_LENGTH} COLORS.")
+            continue
 
-    for color in guess:
-        if color not in colors:
-            print(f"Invalid color: {color}. Try again.")
+        for color in guess:
+            if color not in COLORS:
+                print(f"Invalid color: {color}. Try again.")
+                break
+        else:
             break
-    else:
-        break
 
     return guess
 
 def check_code (guess,real_code):
     color_counts = {}
-        
+    correct_pos = 0
+    incorrect_pos = 0
+
+    for color in real_color:
+        if color not in color_counts:
+            color_counts[color] = 0
+        color_counts[color] += 1
+
+    for guess_color, real_color in zip(guess,real_code):
+        if guess_color == real_color:
+            correct_pos += 1
+            color_counts[guess_color] -= 1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color in color_counts and color_counts[guess_color] > 0:
+            incorrect_pos += 1
+            color_counts[guess_color] -= 1
+
+    return correct_pos, incorrect_pos
+def game():
+    print(f"Welcome to mastermind, tou have {TRIES}to guess the code...")
+    print("The valid colors are", *COLORS)
+    code = generate_code()
+    for attempts in range(1, TRIES+1):
+        guess = guess_code()
+        correct_pos, incorrect_pos = check_code(guess, code)
+
+        if correct_pos == CODE_LENGTH:
+            print(f"You guessed the code in {attempts} TRIES!")
+            break
+        print(f"Correct Positions: {correct_pos} | Incorrect positions: {incorrect_pos}")
+
+    else:
+        print("You ran out of TRIES, the code was:",*code)
+
+if __name__ == "__main__":
+    game()
+
+
 
             
 
